91ccc0f54a0ddd3f598acf9e4838e136f3e7523f
Junaid <junaidmangalwad.26@gmail.com>
2017-10-11 01:27:55 +0530

    Assignment 14.4

---
+++ app/src/main/java/com/example/jmush/android_session14_assignment_144/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.jmush.android_session14_assignment_144;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Context context;
+    private Activity activity;
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private View view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        context = getApplicationContext();
+        activity = this;
+        Button check_permission = (Button)findViewById(R.id.check_permission);
+        Button request_permission = (Button)findViewById(R.id.request_permission);
+        check_permission.setOnClickListener(this);
+        request_permission.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        view = v;
+
+        int id = v.getId();
+        switch (id){
+            case R.id.check_permission:
+                if (checkPermission()) {
+
+                    Toast.makeText(context,"Permission already granted.",Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    Toast.makeText(context,"Please request permission.",Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.request_permission:
+                if (!checkPermission()) {
+
+                    requestPermission();
+
+                } else {
+
+                    Toast.makeText(context,"Permission already granted.",Toast.LENGTH_LONG).show();
+
+                }
+                break;
+        }
+    }
+
+    private boolean checkPermission(){
+        int result = ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (result == PackageManager.PERMISSION_GRANTED){
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
+    }
+
+    private void requestPermission(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity,Manifest.permission.ACCESS_FINE_LOCATION)){
+
+            Toast.makeText(context,"GPS permission allows us to access location data. Please allow in App Settings for additional functionality.",Toast.LENGTH_LONG).show();
+
+        } else {
+
+            ActivityCompat.requestPermissions(activity,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(context,"Permission Granted, Now you can access location data.",Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    Toast.makeText(context,"Permission Denied, You cannot access location data.",Toast.LENGTH_LONG).show();
+
+                }
+                break;
+        }
+    }
+}
